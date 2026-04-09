bfcc8902c2bdee0d5279b62be2522eb4d551c2b8
Ali Emeksiz <ahmetaliemeksiz@gmail.com>
2026-04-09 19:01:29 +0300

    added classes

--- class/main.js
+++ class/main.js
@@ -30,8 +30,8 @@ class Person{
    set setScore(score){
     this.score += score
    }
-   set setSkills(skills){
-    this.skills.push(skills)
+   set setSkill(skill){
+    this.skills.push(skill)
    }
    getPersonInfo(){
     let fullName = this.getFullName()
@@ -41,7 +41,32 @@ class Person{
     let info = `${fullName} is ${this.age}. He lives ${this.city}, ${this.country}. ${formattedSkills}`
     return info
    }
+
+   static favoriteSkill(){
+    const skills = ["HTML","CSS","JS","REACT","PYTHON","NODE"]
+    const index = Math.floor(Math.random() * skills.length)
+    return skills[index]
+   }
+   static ShowDateTime(){
+    let now = new Date();
+    let year = now.getFullYear()
+    let month = now.getMonth() 
+    let date = now.getDate()
+    let hours = now.getHours()
+    let minutes = now.getMinutes()
+    if(hours<10){
+        hours = '0' + hours
+    }
+    if(minutes<10){
+        minutes = '0' + minutes
+    }
+    let dateMonthYear = date + "." + month + '.' + year
+    let time = hours + ":"+ minutes
+    let fullTime = dateMonthYear + time
+    return fullTime 
 }
+}
+
 const person = new Person()
 console.log(person)
 
@@ -51,6 +76,45 @@ console.log(person1.getFullName())
 console.log(person1.getScore)
 console.log(person1.getSkills)
 person1.setScore = 15
-person1.setSkills = 'entry Writer'
-person1.setSkills = 'musician'
+person1.setSkill = 'entry Writer'
+person1.setSkill = 'musician'
 console.log(person1.getPersonInfo())
+
+console.log(Person.favoriteSkill())
+console.log(Person.ShowDateTime())
+
+class Student extends Person{
+    constructor (firstname,  lastname, age,country, gender){
+        super(firstname,  lastname, age,country)
+        this.gender = gender 
+    }
+    getPersonInfo(){
+        let fullName = this.getFullName()
+        let skills = 
+            this.skills.length > 0 && this.skills.slice(0,this.skills.length -1)
+            .join(' ,') + ` and ${this.skills[this.skills.length -1 ]} ` 
+        let formattedSkills = skills ? ` He knows ${skills}` : ''
+        let pronoun = this.gender == "Male" ? "he" : "she"
+        let info = ` ${fullName} is ${this.age}. ${pronoun} lives in ${this.city}, ${this.country}. ${formattedSkills}`
+        return info 
+    }
+}
+
+const s1 = new Student('ahmet','durak',28,'usa','male')
+s1.setScore= 1
+s1.setSkill = 'ruby'
+s1.setSkill = 'kotlin'
+s1.setSkill = 'c++'
+s1.setkill = 'asdsad'
+
+const s2 = new Student('son','min', 36 , 'japan' ,'male ')
+s2.setScore= 50 
+s2.setSkill = 'java'
+s2.setSkill = 'writer'
+s2.setSkill = 'manager'
+
+console.log(s1)
+console.log(s1.getPersonInfo())
+console.log(s2)
+console.log(s2.getPersonInfo())
+console.log(s2.getFullName())
